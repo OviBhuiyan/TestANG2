9916d4382c459ec0c77f297056efcaa7dcba30a4
OVI <OVI@DESKTOP-DF5TG5J>
2017-06-25 04:57:29 +0600

    Complete Done crud with Arrey

--- TestANG2/src/app/app.component.js
+++ TestANG2/src/app/app.component.js
@@ -8,14 +8,49 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular ovi';
+        this.title = 'Crud Operation';
+        this.employees = [
+            { name: "ovi", position: "software Engineer" },
+            { name: "shohan", position: "LLB" },
+            { name: "nipu", position: "doctor" }
+        ];
+        this.model = {};
+        this.model2 = {};
+        this.msg = "";
     }
+    AppComponent.prototype.addEmployee = function () {
+        this.employees.push(this.model);
+        this.model = {};
+        this.msg = "Record is successfully added.....";
+    };
+    AppComponent.prototype.deleteEmployee = function (i) {
+        this.employees.splice(i, 1);
+        this.msg = "Record is successfully deleted.....";
+    };
+    AppComponent.prototype.editEmployee = function (i) {
+        this.model2.name = this.employees[i].name;
+        this.model2.position = this.employees[i].position;
+        this.myvalue = i;
+    };
+    AppComponent.prototype.updateEmployee = function () {
+        var k = this.myvalue;
+        for (var i = 0; i < this.employees.length; i++) {
+            if (i == k) {
+                this.employees[i] = this.model2;
+                this.model2 = {};
+                this.msg = "Record is successfully updated.....";
+            }
+        }
+    };
+    AppComponent.prototype.clickMe = function () {
+        this.msg = "";
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        templateUrl: 'app/app.component.html'
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- TestANG2/src/app/app.component.ts
+++ TestANG2/src/app/app.component.ts
@@ -1,9 +1,53 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent  {
-    name = 'Angular ovi';
+
+    title: string = 'Crud Operation';
+
+    employees = [
+        { name: "ovi", position: "software Engineer" },
+         { name: "shohan", position: "LLB" },
+          { name: "nipu", position: "doctor" }
+
+    ];
+
+    model: any = {};
+    model2: any = {};
+    msg: any = "";
+    addEmployee() {
+        this.employees.push(this.model);
+        this.model = {};
+        this.msg = "Record is successfully added.....";
+    }
+    deleteEmployee(i) {
+        this.employees.splice(i, 1);
+        this.msg = "Record is successfully deleted.....";
+    }
+    myvalue;
+    editEmployee(i) {
+        this.model2.name = this.employees[i].name;
+        this.model2.position = this.employees[i].position;
+        this.myvalue = i;
+    }
+    updateEmployee() {
+        let k = this.myvalue;
+        for (let i = 0; i < this.employees.length; i++)
+        {
+            if (i == k) {
+                this.employees[i] = this.model2;
+                this.model2 = {};
+                this.msg = "Record is successfully updated.....";
+            }
+        }
+    }
+
+
+    clickMe() {
+        this.msg = "";
+    }
+
 }
